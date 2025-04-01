ac33eff097e9de9da3b6727107f8444114dc3d06
arzaluz-chris <23008381@es.uveg.edu.mx>
2025-03-31 20:44:46 -0600

    Initial Commit

---
+++ SunblockAI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SunblockAI
+//
+//  Created by Christian Arzaluz on 31/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SunblockAI/SunblockAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SunblockAIApp.swift
+//  SunblockAI
+//
+//  Created by Christian Arzaluz on 31/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SunblockAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
